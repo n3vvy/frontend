2458fbf18dbb0f231201d894120172a7f83250b5
LonnPiotr <lonnekxd@gmail.com>
2023-05-16 23:03:27 +0200

    Font fix

--- src/components/form/Navbar.js
+++ src/components/form/Navbar.js
@@ -14,6 +14,7 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 
+
 //TEST
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
